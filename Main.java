a93d1798b17afc50bb3ee5f83c2a2eab8c55da58
AMINE AHMIRI <amineahmiri@outlook.com>
2020-05-28 06:07:48 -0400

    I changed Main.java to show that I want to learn Linux

--- Main.java
+++ Main.java
@@ -1,5 +1,6 @@
 public class Main{
 	public static void main(String[] args){
-		System.out.println("My favorite programming is C++ ");
+		System.out.println(" I'm in CS407 because I want to learn Linux ");
+
 	}
 }
